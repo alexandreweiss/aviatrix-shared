87d898ceed2029db463deff36712c2679a0f1f09
Alex Weiss <alexandreweiss@hotmail.com>
2023-04-28 16:18:18 +0200

    enabled microseg for test

--- mcna-lab/dataplane/secu-dfw.tf
+++ mcna-lab/dataplane/secu-dfw.tf
@@ -1,98 +1,113 @@
 // Enable DFW
-# resource "aviatrix_distributed_firewalling_config" "dfw" {
-#   enable_distributed_firewalling = true
-# }
+resource "aviatrix_distributed_firewalling_config" "dfw" {
+  enable_distributed_firewalling = true
+}
+
+// Enable VPC for MicroSeg
+resource "aviatrix_distributed_firewalling_intra_vpc" "app-vnet" {
+  vpcs {
+    account_name = local.accounts.azure_account
+    vpc_id       = "${azurerm_virtual_network.r1-spoke-app.name}:${azurerm_resource_group.azr-r1-spoke-microseg-rg.name}:${azurerm_virtual_network.r1-spoke-app.guid}"
+    region       = var.azure_r1_location
+  }
+}
 
 // Smart Groups
-# resource "aviatrix_smart_group" "catch-all" {
-#   name = "catch-all"
-#   selector {
-#     match_expressions {
-#       cidr = "0.0.0.0/0"
-#     }
-#   }
-# }
+resource "aviatrix_smart_group" "alex-catch-all" {
+  name = "alex-catch-all"
+  selector {
+    match_expressions {
+      cidr = "0.0.0.0/0"
+    }
+  }
+}
 
-# resource "aviatrix_smart_group" "ferme" {
-#   name = "ferme"
-#   selector {
-#     match_expressions {
-#       cidr = "192.168.16.0/24"
-#     }
-#   }
-# }
+resource "aviatrix_smart_group" "ferme" {
+  name = "ferme"
+  selector {
+    match_expressions {
+      cidr = "192.168.16.0/24"
+    }
+  }
+}
 
-# resource "aviatrix_smart_group" "app1-front" {
-#   name = "app1-front"
-#   selector {
-#     match_expressions {
-#       type = "vm"
-#       tags = {
-#         environment = "app1-front"
-#       }
-#     }
-#   }
-# }
+resource "aviatrix_smart_group" "front-app" {
+  name = "front-app"
+  selector {
+    match_expressions {
+      type = "vm"
+      tags = {
+        environment = "front-app"
+      }
+    }
+  }
+}
 
-# resource "aviatrix_smart_group" "app2-front" {
-#   name = "app2-front"
-#   selector {
-#     match_expressions {
-#       type = "vm"
-#       tags = {
-#         environment = "app2-front"
-#       }
-#     }
-#   }
-# }
+resource "aviatrix_smart_group" "sc-app" {
+  name = "sc-app"
+  selector {
+    match_expressions {
+      type = "vm"
+      tags = {
+        environment = "sc-app"
+      }
+    }
+  }
+}
 
-# resource "aviatrix_smart_group" "dev" {
-#   name = "dev"
-#   selector {
-#     match_expressions {
-#       type = "vm"
-#       tags = {
-#         environment = "dev"
-#       }
-#     }
-#   }
-# }
+resource "aviatrix_smart_group" "sql-app" {
+  name = "sql-app"
+  selector {
+    match_expressions {
+      type = "vm"
+      tags = {
+        environment = "sql-app"
+      }
+    }
+  }
+}
 
-# resource "aviatrix_distributed_firewalling_policy_list" "policy" {
-#   policies {
-#     action = "DENY"
-#     src_smart_groups = [ aviatrix_smart_group.dev.uuid ]
-#     name = "DenyDevApp1"
-#     protocol = "ANY"
-#     dst_smart_groups = [ aviatrix_smart_group.app1-front.uuid ]
-#     logging = true
-#     priority = 100
-#   }
-#   policies {
-#     action = "DENY"
-#     src_smart_groups = [ aviatrix_smart_group.app1-front.uuid ]
-#     name = "DenyApp1ToApp2"
-#     protocol = "ANY"
-#     dst_smart_groups = [ aviatrix_smart_group.app2-front.uuid ]
-#     logging = true
-#     priority = 200
-#   }
-#   policies {
-#     action = "PERMIT"
-#     src_smart_groups = [ aviatrix_smart_group.ferme.uuid ]
-#     name = "AllowFerme"
-#     protocol = "ANY"
-#     dst_smart_groups = [ aviatrix_smart_group.catch-all.uuid ]
-#     logging = true
-#     priority = 3500
-#   }
-#   policies {
-#     action = "PERMIT"
-#     src_smart_groups = [ aviatrix_smart_group.catch-all.uuid ]
-#     name = "DenyAll"
-#     protocol = "ANY"
-#     dst_smart_groups = [ aviatrix_smart_group.catch-all.uuid ]
-#     logging = true
-#     priority = 4000
-#   }
-# }
+resource "aviatrix_distributed_firewalling_policy_list" "policy" {
+  policies {
+    action           = "PERMIT"
+    src_smart_groups = [aviatrix_smart_group.front-app.uuid]
+    name             = "AllowFrontToSc"
+    protocol         = "TCP"
+    port_ranges {
+      lo = 443
+    }
+    dst_smart_groups = [aviatrix_smart_group.sc-app.uuid]
+    logging          = true
+    priority         = 100
+  }
+  policies {
+    action           = "PERMIT"
+    src_smart_groups = [aviatrix_smart_group.sc-app.uuid]
+    name             = "AllowScToSql"
+    protocol         = "TCP"
+    port_ranges {
+      lo = 1433
+    }
+    dst_smart_groups = [aviatrix_smart_group.sql-app.uuid]
+    logging          = true
+    priority         = 200
+  }
+  # policies {
+  #   action           = "PERMIT"
+  #   src_smart_groups = [aviatrix_smart_group.ferme.uuid]
+  #   name             = "AllowFerme"
+  #   protocol         = "ANY"
+  #   dst_smart_groups = [aviatrix_smart_group.catch-all.uuid]
+  #   logging          = true
+  #   priority         = 3500
+  # }
+  # policies {
+  #   action           = "PERMIT"
+  #   src_smart_groups = [aviatrix_smart_group.catch-all.uuid]
+  #   name             = "DenyAll"
+  #   protocol         = "ANY"
+  #   dst_smart_groups = [aviatrix_smart_group.catch-all.uuid]
+  #   logging          = true
+  #   priority         = 4000
+  # }
+}
--- mcna-lab/dataplane/transits.tf
+++ mcna-lab/dataplane/transits.tf
@@ -2,18 +2,16 @@ module "azure_transit_we" {
   source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
   version = "2.4.0"
 
-  cloud                  = "azure"
-  region                 = var.azure_r1_location
-  cidr                   = "10.10.0.0/23"
-  account                = local.accounts.azure_account
-  enable_transit_firenet = true
-  name                   = "azr-${var.azure_r1_location_short}-transit"
-  local_as_number        = 65007
-  //enable_segmentation           = true
+  cloud                         = "azure"
+  region                        = var.azure_r1_location
+  cidr                          = "10.10.0.0/23"
+  account                       = local.accounts.azure_account
+  enable_transit_firenet        = true
+  name                          = "azr-${var.azure_r1_location_short}-transit"
+  local_as_number               = 65007
   enable_advertise_transit_cidr = true
   single_az_ha                  = false
   resource_group                = azurerm_resource_group.azr-transit-r1-0-rg.name
-  //instance_size = "Standard_B2ms"
 }
 
 module "azure_transit_we_egress" {
