d2e8881a6ac049bb7c3daa4f3487018339c2f999
Alex Weiss <alexandreweiss@hotmail.com>
2024-07-31 17:44:21 +0200

    remoked secret and deleted file Aviatrix license revoked

--- misc/aviatrix-launch-aws.tf
+++
@@ -1,8 +0,0 @@
-module "basic" {
-  source            = "github.com/aviatrix/terraform-aviatrix-aws-controlplane?ref=dev"
-  incoming_ssl_cidr = ["81.49.43.155/32"]
-  admin_email       = "aweiss@aviatrix.com"
-  avx_password      = "pMR%35*>SC?VPwMh"
-  avx_customer_id   = "aviatrix.com-abu-e8ea5d00-1689194403.159335"
-  region            = "eu-central-1"
-}
