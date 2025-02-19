49d186376ebd84b6cdd08fc96b1e0bfa8e8ea6c3
Alex Weiss <alexandreweiss@hotmail.com>
2025-02-19 22:47:30 +0000

    backup

--- aws-lab/main.tf
+++ aws-lab/main.tf
@@ -38,9 +38,10 @@ resource "aws_route" "route" {
 
 #Create subnets and associate created route tables
 resource "aws_subnet" "subnet" {
-  for_each   = toset(["subnet-internal-a", "subnet-internal-b", "subnet-public-a", "subnet-public-b", "subnet-internal-bgp-lan-a", "subnet-internal-bgp-lan-b"])
-  vpc_id     = aws_vpc.vpc_r1.id
-  cidr_block = each.key == "subnet-internal-a" ? cidrsubnet(var.vpc_cidr, 4, 0) : each.key == "subnet-internal-b" ? cidrsubnet(var.vpc_cidr, 4, 1) : each.key == "subnet-public-a" ? cidrsubnet(var.vpc_cidr, 4, 2) : each.key == "subnet-internal-bgp-lan-a" ? cidrsubnet(var.vpc_cidr, 4, 3) : each.key == "subnet-internal-bgp-lan-b" ? cidrsubnet(var.vpc_cidr, 4, 4) : cidrsubnet(var.vpc_cidr, 4, 5)
+  for_each          = toset(["subnet-internal-a", "subnet-internal-b", "subnet-public-a", "subnet-public-b", "subnet-internal-bgp-lan-a", "subnet-internal-bgp-lan-b"])
+  vpc_id            = aws_vpc.vpc_r1.id
+  cidr_block        = each.key == "subnet-internal-a" ? cidrsubnet(var.vpc_cidr, 4, 0) : each.key == "subnet-internal-b" ? cidrsubnet(var.vpc_cidr, 4, 1) : each.key == "subnet-public-a" ? cidrsubnet(var.vpc_cidr, 4, 2) : each.key == "subnet-public-b" ? cidrsubnet(var.vpc_cidr, 4, 3) : each.key == "subnet-internal-bgp-lan-a" ? cidrsubnet(var.vpc_cidr, 4, 4) : cidrsubnet(var.vpc_cidr, 4, 5)
+  availability_zone = each.key == "subnet-internal-a" || each.key == "subnet-public-a" || each.key == "subnet-internal-bgp-lan-a" ? "${var.aws_r1_location}a" : "${var.aws_r1_location}b"
 
   tags = {
     Name = format("%s-%s", "${var.aws_r1_location_short}-transit-${var.customer_name}", each.key)
@@ -49,10 +50,10 @@ resource "aws_subnet" "subnet" {
 
 #Associate route tables to subnets
 resource "aws_route_table_association" "rt-association" {
-  for_each = { for k, v in aws_subnet.subnet : k => v if length(regexall("subnet-.*", k)) == 0 }
+  for_each = aws_subnet.subnet
 
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.rt[each.key].id
+  route_table_id = aws_route_table.rt[replace(each.key, "subnet-", "rt-")].id
 }
 
 
@@ -64,14 +65,14 @@ resource "aws_ec2_transit_gateway" "tgw" {
   }
 }
 
-# #Create a TGW attachment to VPC
-# resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_transit_attach" {
-#   subnet_ids         = [aws_subnet.subnet["subnet-public-a"].id, aws_subnet.subnet["subnet-public-b"].id]
-#   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-#   vpc_id             = aws_vpc.vpc_r1.id
-# }
+#Create a TGW attachment to VPC
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_transit_attach" {
+  subnet_ids         = [aws_subnet.subnet["subnet-internal-bgp-lan-a"].id, aws_subnet.subnet["subnet-internal-bgp-lan-b"].id]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  vpc_id             = aws_vpc.vpc_r1.id
+}
 
-# #Create Virtual Private Gateway
+#Create Virtual Private Gateway
 # resource "aws_vpn_gateway" "vgw" {
 #   vpc_id          = aws_vpc.vpc_r1.id
 #   amazon_side_asn = 64901
@@ -80,15 +81,15 @@ resource "aws_ec2_transit_gateway" "tgw" {
 #   }
 # }
 
-# #Create a Direct Connect Gateway
-# resource "aws_dx_gateway" "dxgw" {
-#   amazon_side_asn = 64900
-#   name            = format("%s-dxgw", "${var.aws_r1_location_short}-${var.customer_name}-dxgw")
-# }
+#Create a Direct Connect Gateway
+resource "aws_dx_gateway" "dxgw" {
+  amazon_side_asn = 64900
+  name            = format("%s-dxgw", "${var.aws_r1_location_short}-${var.customer_name}-dxgw")
+}
 
-# #Create a Direct Connect Gateway Association
-# resource "aws_dx_gateway_association" "dxgw_assoc" {
-#   dx_gateway_id = aws_dx_gateway.dxgw.id
-#   associated_gateway_id = aws_vpn_gateway.vgw.id
-#   vpc_id        = aws_vpc.vpc_r1.id
-# }
+#Create a Direct Connect Gateway Association
+resource "aws_dx_gateway_association" "dxgw_assoc" {
+  dx_gateway_id         = aws_dx_gateway.dxgw.id
+  associated_gateway_id = aws_ec2_transit_gateway.tgw.id
+  allowed_prefixes      = ["0.0.0.0/0"]
+}
